8948300e903ae01811733a18fb36b86266aa26f3
Dynamic Programming <amicming@gmail.com>
2012-03-13 18:39:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactUs::Application.config.secret_token = '2783af739b1e3b9299b90e7a575833f1510384fe15d7dd0f2007b8e27a806aeb118b6ae01fab704209fafaade6bb0ea3f7db33795b7755bdad250c15320f823d'
