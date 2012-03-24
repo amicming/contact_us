50ba7190446dc0aea7672a9b41d5a4d3ce953434
Dynamic Programming <amicming@gmail.com>
2012-03-24 15:11:17 -0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ContactUs::Application.routes.draw do
   get "pages/home"
-
   get "pages/help"
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
 describe "Pages" do
-  describe "GET /pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get pages_index_path
-      response.status.should be(200)
-    end
-  end
+	describe "Home page" do
+		it "should have the h1 'Sample App'" do
+			visit '/pages/home'
+			page.should have_selector('h1', :text => 'Sample App')
+		end
+		it "should have the title 'Home'" do
+			visit '/pages/home'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+		end
+	end
+
+	describe "Help page" do
+		it "should have the h1 'Help'" do
+			visit '/pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+		it "should have the title 'Help'" do
+			visit '/pages/help'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+		end
+	end
+	
+	describe "About page" do
+		it "should have the h1 'About Us'" do
+			visit '/pages/about'
+			page.should have_selector('h1', :text => 'About Us')
+		end
+		it "should have the title 'About Us'" do
+			visit '/pages/about'
+			page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+		end
+	end
 end
